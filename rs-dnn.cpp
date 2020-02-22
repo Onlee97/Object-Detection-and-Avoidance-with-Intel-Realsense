4327f962d322bd9853bcbb38cad91f26d37e6ef3
Nhi Tran <mainhitranthien@gmail.com>
2020-02-22 13:37:51 -0500

    Add if-else to detect 1 type of object only

--- rs-dnn.cpp
+++ rs-dnn.cpp
@@ -6,17 +6,17 @@
 #include <librealsense2/rs.hpp>
 #include "../cv-helpers.hpp"
 
-const size_t inWidth      = 300;
-const size_t inHeight     = 300;
-const float WHRatio       = inWidth / (float)inHeight;
+const size_t inWidth = 300;
+const size_t inHeight = 300;
+const float WHRatio = inWidth / (float)inHeight;
 const float inScaleFactor = 0.007843f;
-const float meanVal       = 127.5;
-const char* classNames[]  = {"background",
+const float meanVal = 127.5;
+const char* classNames[] = { "background",
                              "aeroplane", "bicycle", "bird", "boat",
                              "bottle", "bus", "car", "cat", "chair",
                              "cow", "diningtable", "dog", "horse",
                              "motorbike", "person", "pottedplant",
-                             "sheep", "sofa", "train", "tvmonitor"};
+                             "sheep", "sofa", "train", "tvmonitor" };
 
 int main(int argc, char** argv) try
 {
@@ -24,31 +24,31 @@ int main(int argc, char** argv) try
     using namespace cv::dnn;
     using namespace rs2;
 
-    Net net = readNetFromCaffe("MobileNetSSD_deploy.prototxt", 
-                               "MobileNetSSD_deploy.caffemodel");
+    Net net = readNetFromCaffe("MobileNetSSD_deploy.prototxt",
+        "MobileNetSSD_deploy.caffemodel");
 
     // Start streaming from Intel RealSense Camera
     pipeline pipe;
     auto config = pipe.start();
     auto profile = config.get_stream(RS2_STREAM_COLOR)
-                         .as<video_stream_profile>();
+        .as<video_stream_profile>();
     rs2::align align_to(RS2_STREAM_COLOR);
 
     Size cropSize;
     if (profile.width() / (float)profile.height() > WHRatio)
     {
         cropSize = Size(static_cast<int>(profile.height() * WHRatio),
-                        profile.height());
+            profile.height());
     }
     else
     {
         cropSize = Size(profile.width(),
-                        static_cast<int>(profile.width() / WHRatio));
+            static_cast<int>(profile.width() / WHRatio));
     }
 
     Rect crop(Point((profile.width() - cropSize.width) / 2,
-                    (profile.height() - cropSize.height) / 2),
-              cropSize);
+        (profile.height() - cropSize.height) / 2),
+        cropSize);
 
     const auto window_name = "Display Image";
     namedWindow(window_name, WINDOW_AUTOSIZE);
@@ -74,7 +74,7 @@ int main(int argc, char** argv) try
         auto depth_mat = depth_frame_to_meters(pipe, depth_frame);
 
         Mat inputBlob = blobFromImage(color_mat, inScaleFactor,
-                                      Size(inWidth, inHeight), meanVal, false); //Convert Mat to batch of images
+            Size(inWidth, inHeight), meanVal, false); //Convert Mat to batch of images
         net.setInput(inputBlob, "data"); //set the network input
         Mat detection = net.forward("detection_out"); //compute output
 
@@ -85,11 +85,11 @@ int main(int argc, char** argv) try
         depth_mat = depth_mat(crop);
 
         float confidenceThreshold = 0.8f;
-        for(int i = 0; i < detectionMat.rows; i++)
+        for (int i = 0; i < detectionMat.rows; i++)
         {
             float confidence = detectionMat.at<float>(i, 2);
 
-            if(confidence > confidenceThreshold)
+            if (confidence > confidenceThreshold)
             {
                 size_t objectClass = (size_t)(detectionMat.at<float>(i, 1));
 
@@ -99,10 +99,10 @@ int main(int argc, char** argv) try
                 int yRightTop = static_cast<int>(detectionMat.at<float>(i, 6) * color_mat.rows);
 
                 Rect object((int)xLeftBottom, (int)yLeftBottom,
-                            (int)(xRightTop - xLeftBottom),
-                            (int)(yRightTop - yLeftBottom));
+                    (int)(xRightTop - xLeftBottom),
+                    (int)(yRightTop - yLeftBottom));
 
-                object = object  & Rect(0, 0, depth_mat.cols, depth_mat.rows);
+                object = object & Rect(0, 0, depth_mat.cols, depth_mat.rows);
 
                 // Calculate mean depth inside the detection region
                 // This is a very naive way to estimate objects depth
@@ -111,22 +111,27 @@ int main(int argc, char** argv) try
                 Scalar m = mean(depth_mat(object));
 
                 std::ostringstream ss;
-                ss << classNames[objectClass] << " ";
-                ss << std::setprecision(2) << m[0] << " meters away";
+                if (classNames[objectClass] == "person") {
+                    ss << classNames[objectClass] << " ";
+                    ss << std::setprecision(2) << m[0] << " meters away";
+                }
+                else {
+                    ss << "";
+                }
                 String conf(ss.str());
 
                 rectangle(color_mat, object, Scalar(0, 255, 0));
                 int baseLine = 0;
                 Size labelSize = getTextSize(ss.str(), FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
 
-                auto center = (object.br() + object.tl())*0.5;
+                auto center = (object.br() + object.tl()) * 0.5;
                 center.x = center.x - labelSize.width / 2;
 
                 rectangle(color_mat, Rect(Point(center.x, center.y - labelSize.height),
                     Size(labelSize.width, labelSize.height + baseLine)),
                     Scalar(255, 255, 255), FILLED);
                 putText(color_mat, ss.str(), center,
-                        FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0,0,0));
+                    FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 0, 0));
             }
         }
 
@@ -141,7 +146,7 @@ catch (const rs2::error & e)
     std::cerr << "RealSense error calling " << e.get_failed_function() << "(" << e.get_failed_args() << "):\n    " << e.what() << std::endl;
     return EXIT_FAILURE;
 }
-catch (const std::exception& e)
+catch (const std::exception & e)
 {
     std::cerr << e.what() << std::endl;
     return EXIT_FAILURE;
