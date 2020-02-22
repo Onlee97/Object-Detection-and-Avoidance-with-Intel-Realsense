589ebe6375a0cf8c01387dd0b83681ffdeab30fe
Tran <TranN@dvaco.org>
2020-02-21 23:26:34 -0500

    Create rs-dnn file using YOLO net

---
+++ rs-dnn-yolo.cpp
@@ -0,0 +1,153 @@
+// This example is derived from the ssd_mobilenet_object_detection opencv demo
+// and adapted to be used with Intel RealSense Cameras
+// Please see https://github.com/opencv/opencv/blob/master/LICENSE
+
+#include <opencv2/dnn.hpp>
+#include <librealsense2/rs.hpp>
+#include "../cv-helpers.hpp"
+
+int main(int argc, char** argv) try
+{
+    using namespace cv;
+    using namespace cv::dnn;
+    using namespace rs2;
+	
+{
+    CommandLineParser parser(argc, argv, params);
+
+    if (parser.get<bool>("help"))
+    {
+        cout << about << endl;
+        parser.printMessage();
+        return 0;
+    }
+
+    String modelConfiguration = parser.get<String>("cfg");
+    String modelBinary = parser.get<String>("model");
+
+    Net net = readNetFromDarknet(modelConfiguration, modelBinary);
+
+    // Start streaming from Intel RealSense Camera
+    pipeline pipe;
+    auto config = pipe.start();
+    auto profile = config.get_stream(RS2_STREAM_COLOR)
+                         .as<video_stream_profile>();
+    rs2::align align_to(RS2_STREAM_COLOR);
+
+    Size cropSize;
+    if (profile.width() / (float)profile.height() > WHRatio)
+    {
+        cropSize = Size(static_cast<int>(profile.height() * WHRatio),
+                        profile.height());
+    }
+    else
+    {
+        cropSize = Size(profile.width(),
+                        static_cast<int>(profile.width() / WHRatio));
+    }
+
+    Rect crop(Point((profile.width() - cropSize.width) / 2,
+                    (profile.height() - cropSize.height) / 2),
+              cropSize);
+
+    const auto window_name = "Display Image";
+    namedWindow(window_name, WINDOW_AUTOSIZE);
+
+    while (getWindowProperty(window_name, WND_PROP_AUTOSIZE) >= 0)
+    {
+        // Wait for the next set of frames
+        auto data = pipe.wait_for_frames();
+        // Make sure the frames are spatially aligned
+        data = align_to.process(data);
+
+        auto color_frame = data.get_color_frame();
+        auto depth_frame = data.get_depth_frame();
+
+        // If we only received new depth frame, 
+        // but the color did not update, continue
+        static int last_frame_number = 0;
+        if (color_frame.get_frame_number() == last_frame_number) continue;
+        last_frame_number = color_frame.get_frame_number();
+
+        // Convert RealSense frame to OpenCV matrix:
+        auto color_mat = frame_to_mat(color_frame);
+        auto depth_mat = depth_frame_to_meters(pipe, depth_frame);
+
+        Mat inputBlob = blobFromImage(color_mat, inScaleFactor,
+                                      Size(inWidth, inHeight), meanVal, false); //Convert Mat to batch of images
+        net.setInput(inputBlob, "data"); //set the network input
+        Mat detectionMat = net.forward("detection_out"); //compute output
+
+        // Crop both color and depth frames
+        color_mat = color_mat(crop);
+        depth_mat = depth_mat(crop);
+
+        float confidenceThreshold = 0.8f;
+        for(int i = 0; i < detectionMat.rows; i++)
+        {
+            const int probability_index = 5;
+            const int probability_size = detectionMat.cols - probability_index;
+            float *prob_array_ptr = &detectionMat.at<float>(i, probability_index);
+
+            size_t objectClass = max_element(prob_array_ptr, prob_array_ptr + probability_size) - prob_array_ptr;
+            float confidence = detectionMat.at<float>(i, (int)objectClass + probability_index);
+
+            if (confidence > confidenceThreshold)
+            {
+                float x = detectionMat.at<float>(i, 0);
+                float y = detectionMat.at<float>(i, 1);
+                float width = detectionMat.at<float>(i, 2);
+                float height = detectionMat.at<float>(i, 3);
+                int xLeftBottom = static_cast<int>((x - width / 2) * frame.cols);
+                int yLeftBottom = static_cast<int>((y - height / 2) * frame.rows);
+                int xRightTop = static_cast<int>((x + width / 2) * frame.cols);
+                int yRightTop = static_cast<int>((y + height / 2) * frame.rows);
+
+                Rect object(xLeftBottom, yLeftBottom,
+                            xRightTop - xLeftBottom,
+                            yRightTop - yLeftBottom);
+
+                rectangle(frame, object, Scalar(0, 255, 0));
+
+                if (objectClass < classNamesVec.size())
+                {
+                    ss.str("");
+                    ss << confidence;
+                    String conf(ss.str());
+                    String label = String(classNamesVec[objectClass]) + ": " + conf;
+                    int baseLine = 0;
+                    Size labelSize = getTextSize(label, FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
+                    rectangle(frame, Rect(Point(xLeftBottom, yLeftBottom ),
+                                          Size(labelSize.width, labelSize.height + baseLine)),
+                              Scalar(255, 255, 255), CV_FILLED);
+                    putText(frame, label, Point(xLeftBottom, yLeftBottom+labelSize.height),
+                            FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0,0,0));
+                }
+                else
+                {
+                    cout << "Class: " << objectClass << endl;
+                    cout << "Confidence: " << confidence << endl;
+                    cout << " " << xLeftBottom
+                         << " " << yLeftBottom
+                         << " " << xRightTop
+                         << " " << yRightTop << endl;
+                }
+            }
+		}
+
+        imshow("YOLO: Detections", frame);
+        if (waitKey(1) >= 0) break;
+    }
+
+    return EXIT_SUCCESS;
+}
+catch (const rs2::error & e)
+{
+    std::cerr << "RealSense error calling " << e.get_failed_function() << "(" << e.get_failed_args() << "):\n    " << e.what() << std::endl;
+    return EXIT_FAILURE;
+}
+catch (const std::exception& e)
+{
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+}
