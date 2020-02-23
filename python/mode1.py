0be22c976226b9efd96c1ddc95ba421b4f2b6531
Onlee97 <lekhacduy97@gmail.com>
2020-02-23 12:13:57 -0500

    mode1 to change

--- python/mode1.py
+++ python/mode1.py
@@ -23,26 +23,88 @@ def startRsPipeline():
     pipeline.start(config)
     return pipeline
 
+delayTime = 0.5
+def beepLeft(duration):
+	freq = 440
+	#os.system('play -nq -t alsa synth {} sine {}'.format(duration, freq))
+	os.system('spd-say "left"')
+	time.sleep(delayTime)
+
+def beepRight(duration):
+	freq = 1000
+	#os.system('play -nq -t alsa synth {} sine {}'.format(duration, freq))
+	os.system('spd-say "right"')
+	time.sleep(delayTime)
+
+def loop(pipeline):
+	width = 640
+	height = 360
+	frames = pipeline.wait_for_frames()
+	depth_frame = frames.get_depth_frame()
+	color_frame = frames.get_color_frame()
+	if not depth_frame or not color_frame:
+		return
+
+	# Print a simple text-based representation of the image, by breaking it into 10x20 pixel regions and approximating the coverage of pixels within one meter
+	coverage = [0]*64
+
+	#Left search
+	minDepthLeft = width
+	minXLeft = 0
+	step = 5
+
+	leftThreshold = int(width/5)
+
+	for x in range(leftThreshold,int(width/2),step):			
+		for y in range(0,int(height), step):
+			dist = depth_frame.get_distance(x, y)
+			if (dist != 0 and dist < minDepthLeft):
+				minDepthLeft = dist
+				minXLeft = x
+
+	#Right search
+	minDepthRight = width
+	minXRight = 0
+
+	rightThreshold = int(width*4/5)
+	for x in range(int(width/2),rightThreshold, step):			
+		for y in range(0,int(height), step):
+			dist = depth_frame.get_distance(x, y)
+			if (dist != 0 and dist < minDepthRight):
+				minDepthRight = dist
+				minXRight = x
+
+	depthThreshold = 0.5
+	# print(minDepthRight, minDepthLeft)
+	# if minDepthRight < depthThreshold and minDepthRight < depthThreshold:
+	# 	print("Right: ", minDepthRight, " | Left: ", minDepthLeft)
+	if minDepthRight < depthThreshold:
+		print("Right: ", minDepthRight)
+		beepRight(minDepthRight)
+	if minDepthLeft < depthThreshold:
+		print("Left: ", minDepthLeft)
+		beepLeft(minDepthLeft)
+	# depth_image = np.asanyarray(depth_frame.get_data())
+	# color_image = np.asanyarray(color_frame.get_data())
+
+	# # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
+	# depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+
+	# # Stack both images horizontally
+	# # images = np.hstack((color_image, depth_colormap))
+	# images = depth_colormap
+	# # Show images
+	# # cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+	# cv2.imshow('RealSense', images)
+	# cv2.waitKey(1)
 
 def main(pipeline = None):
 	if pipeline == None:
 		pipeline = startRsPipeline()
 
-	delayTime = 0.5
-	def beepLeft(duration):
-		freq = 440
-		#os.system('play -nq -t alsa synth {} sine {}'.format(duration, freq))
-		os.system('spd-say "left"')
-		time.sleep(delayTime)
+	
 
-	def beepRight(duration):
-		freq = 1000
-		#os.system('play -nq -t alsa synth {} sine {}'.format(duration, freq))
-		os.system('spd-say "right"')
-		time.sleep(delayTime)
 
-	width = 640
-	height = 360
 	
 	try:
 		# Create a context object. This object owns the handles to all connected realsense devices
@@ -50,65 +112,7 @@ def main(pipeline = None):
 		while True:
 			# This call waits until a new coherent set of frames is available on a device
 			# Calls to get_frame_data(...) and get_frame_timestamp(...) on a device will return stable values until wait_for_frames(...) is called
-			frames = pipeline.wait_for_frames()
-			depth_frame = frames.get_depth_frame()
-			color_frame = frames.get_color_frame()
-			if not depth_frame or not color_frame:
-				continue
-
-			# Print a simple text-based representation of the image, by breaking it into 10x20 pixel regions and approximating the coverage of pixels within one meter
-			coverage = [0]*64
-
-			#Left search
-			minDepthLeft = width
-			minXLeft = 0
-			step = 5
-
-			leftThreshold = int(width/5)
-
-			for x in range(leftThreshold,int(width/2),step):			
-				for y in range(0,int(height), step):
-					dist = depth_frame.get_distance(x, y)
-					if (dist != 0 and dist < minDepthLeft):
-						minDepthLeft = dist
-						minXLeft = x
-
-			#Right search
-			minDepthRight = width
-			minXRight = 0
-
-			rightThreshold = int(width*4/5)
-			for x in range(int(width/2),rightThreshold, step):			
-				for y in range(0,int(height), step):
-					dist = depth_frame.get_distance(x, y)
-					if (dist != 0 and dist < minDepthRight):
-						minDepthRight = dist
-						minXRight = x
-
-			depthThreshold = 0.5
-			# print(minDepthRight, minDepthLeft)
-			# if minDepthRight < depthThreshold and minDepthRight < depthThreshold:
-			# 	print("Right: ", minDepthRight, " | Left: ", minDepthLeft)
-			if minDepthRight < depthThreshold:
-				print("Right: ", minDepthRight)
-				beepRight(minDepthRight)
-			if minDepthLeft < depthThreshold:
-				print("Left: ", minDepthLeft)
-				beepLeft(minDepthLeft)
-			# depth_image = np.asanyarray(depth_frame.get_data())
-			# color_image = np.asanyarray(color_frame.get_data())
-
-			# # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
-			# depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
-
-			# # Stack both images horizontally
-			# # images = np.hstack((color_image, depth_colormap))
-			# images = depth_colormap
-			# # Show images
-			# # cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
-			# cv2.imshow('RealSense', images)
-			# cv2.waitKey(1)
-
+			loop(pipeline)
 
 		exit(0)
 	#except rs.error as e:
