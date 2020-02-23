3954a5274d00ebf8a51b88ecc9eeff2c53dc67af
Onlee97 <lekhacduy97@gmail.com>
2020-02-22 18:35:35 -0800

    CUDA might works

---
+++ GPU/build/CMakeFiles/capture.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/capture.dir/test.cpp.o"
+  "capture.pdb"
+  "capture"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/capture.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
