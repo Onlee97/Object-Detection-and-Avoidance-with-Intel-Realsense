86a2b9964d3d2bbd38ae644617307b387947a819
Onlee97 <lekhacduy97@gmail.com>
2020-02-22 19:40:13 -0500

    CMake works for OpenCv

---
+++ cmake/test.cpp
@@ -0,0 +1,27 @@
+#include <opencv2/opencv.hpp>
+#include <librealsense2/rs.hpp>
+//#include "cv-helpers.hpp"
+using namespace cv;
+int rs(){
+
+
+}
+int main(int argc, char** argv)
+{
+    VideoCapture cap;
+    // open the default camera, use something different from 0 otherwise;
+    // Check VideoCapture documentation.
+    if(!cap.open(0))
+        return 0;
+    for(;;)
+    {
+          Mat frame;
+          cap >> frame;
+          if( frame.empty() ) break; // end of video stream
+          imshow("this is you, smile! :)", frame);
+          if( waitKey(10) == 27 ) break; // stop capturing by pressing ESC 
+    }
+    // the camera will be closed automatically upon exit
+    // cap.close();
+    return 0;
+}
