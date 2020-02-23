534470595a1f835da3a3bc9e0970f05abdb475e1
Onlee97 <lekhacduy97@gmail.com>
2020-02-22 20:27:14 -0500

    CMake works for realsense

---
+++ cmake/build/CMakeFiles/rs.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rs.dir/rs2.cpp.o"
+  "rs.pdb"
+  "rs"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake/build/CMakeFiles/rs2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rs2.dir/rs2.cpp.o"
+  "rs2.pdb"
+  "rs2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rs2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
